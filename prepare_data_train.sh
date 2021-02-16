c09d199712762a1587edfc4bd0f3fc9b6ea961f8
Stepan Konev <stepankonev@zomb-launcher.zombie.yandex.net>
2021-02-16 11:40:47 +0300

    add visualization

--- prepare_data_train.sh
+++ prepare_data_train.sh
@@ -23,7 +23,7 @@ popd
 
 pushd src/1st_level
 echo "Prerendering val dataset..."
-python prerender_raster.py --dset_name val --scene_step 36 --skip_frame_step 0 --initial_scenes 0 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27 28 29 30 31 32 33 34 35 --num_jobs 3 --dir_name pre_render_h01248_XXL
+# python prerender_raster.py --dset_name val --scene_step 36 --skip_frame_step 0 --initial_scenes 0 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27 28 29 30 31 32 33 34 35 --num_jobs 3 --dir_name pre_render_h01248_XXL
 # popd
 
 echo "Prerendering train_XXL dataset..."
